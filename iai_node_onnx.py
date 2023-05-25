11f427bacfc445f2f66670286dfa5976d5e2460b
Andy Luhrs <aluhrs@outlook.com>
2023-05-24 21:35:51 -0700

    Minimize node

--- iai_node_onnx.py
+++ iai_node_onnx.py
@@ -4,14 +4,11 @@
 from typing import Literal
 from pydantic import Field
 from invokeai.app.invocations.baseinvocation import BaseInvocation, InvocationContext
-from invokeai.app.invocations.prompt import PromptOutput
-
-import warnings
-from pathlib import Path
+from invokeai.app.invocations.image import ImageOutput, build_image_output
+from invokeai.app.models.image import ImageType
 
 import onnxruntime as ort
 from diffusers import OnnxStableDiffusionPipeline
-from packaging import version
 
 """
 TODO:
@@ -23,75 +20,20 @@ class OnnxPipelineInvocation(BaseInvocation):
     #fmt: off
     type: Literal["onnx_pipeline"] = "onnx_pipeline"
     prompt: str = Field(default=None, description="Prompt")
-    num_images: int = Field(default=1, description="Number of images to generate")
-    batch_size: int = Field(default=1, description="Number of images to generate per batch")
     num_inference_steps: int = Field(default=50, description="Number of steps in diffusion process")
-    dynamic_dims: bool = Field(default=False, description="Disable static shape optimization")
+    static_dims: bool = Field(default=True, description="Disable static shape optimization")
     model_dir: str = Field(default="D:\\Olive\\examples\\directml\\stable_diffusion\\models\\optimized\\runwayml\\stable-diffusion-v1-5", description="Model directory")
     #fmt: on
 
-    def invoke(self, context: InvocationContext) -> PromptOutput:
-        if version.parse(ort.__version__) < version.parse("1.15.0"):
-            print("This script requires onnxruntime-directml 1.15.0 or newer")
-            exit(1)
-
-        use_static_dims = not self.dynamic_dims
-
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self.run_inference(
-                self.model_dir,
-                self.prompt,
-                self.num_images,
-                self.batch_size,
-                self.num_inference_steps,
-                use_static_dims,
-            )
-
-        return PromptOutput(prompt=self.prompt)
-
-
-    def run_inference_loop(
-        self, pipeline, prompt, num_images, batch_size, num_inference_steps, image_callback=None, step_callback=None
-    ):
-        images_saved = 0
-
-        def update_steps(step, timestep, latents):
-            if step_callback:
-                step_callback((images_saved // batch_size) * num_inference_steps + step)
-
-        while images_saved < num_images:
-            print(f"\nInference Batch Start (batch size = {batch_size}).")
-            result = pipeline(
-                [prompt] * batch_size,
-                num_inference_steps=num_inference_steps,
-                callback=update_steps if step_callback else None,
-            )
-            passed_safety_checker = 0
-
-
-            for image_index in range(batch_size):
-                if not result.nsfw_content_detected[image_index]:
-                    passed_safety_checker += 1
-                    if images_saved < num_images:
-                        output_path = f"result_{images_saved}.png"
-                        result.images[image_index].save(output_path)
-                        if image_callback:
-                            image_callback(images_saved, output_path)
-                        images_saved += 1
-                        print(f"Generated {output_path}")
-
-            print(f"Inference Batch End ({passed_safety_checker}/{batch_size} images passed the safety checker).")
-
-
-    def run_inference(self, optimized_model_dir, prompt, num_images, batch_size, num_inference_steps, static_dims):
+    def invoke(self, context: InvocationContext) -> ImageOutput:
         ort.set_default_logger_severity(3)
+        batch_size = 1
 
         print("Loading models into ORT session...")
         sess_options = ort.SessionOptions()
         sess_options.enable_mem_pattern = False
 
-        if static_dims:
+        if self.static_dims:
             # Not necessary, but helps DML EP further optimize runtime performance.
             # batch_size is doubled for sample & hidden state because of classifier free guidance:
             # https://github.com/huggingface/diffusers/blob/46c52f9b9607e6ecb29c782c052aea313e6487b7/src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py#L672
@@ -104,7 +46,23 @@ class OnnxPipelineInvocation(BaseInvocation):
             sess_options.add_free_dimension_override_by_name("unet_hidden_sequence", 77)
 
         pipeline = OnnxStableDiffusionPipeline.from_pretrained(
-            optimized_model_dir, provider="DmlExecutionProvider", sess_options=sess_options
+            self.model_dir, provider="DmlExecutionProvider", sess_options=sess_options
         )
 
-        self.run_inference_loop(pipeline, prompt, num_images, batch_size, num_inference_steps)
+        result = pipeline(
+            [self.prompt] * batch_size,
+            num_inference_steps=self.num_inference_steps,
+        )
+
+        image_type = ImageType.RESULT
+        image_name = context.services.images.create_name(
+            context.graph_execution_state_id, self.id
+        )
+        metadata = context.services.metadata.build_metadata(
+            session_id=context.graph_execution_state_id, node=self
+        )
+        context.services.images.save(image_type, image_name, result.images[0], metadata)
+
+        return build_image_output(
+            image_type=image_type, image_name=image_name, image=result.images[0]
+        )
